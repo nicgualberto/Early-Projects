dca75e11f742444c8470f386db6be2ec91e7d0fe
Nícolas <nicollas.triunfo.pb@gmail.com>
2025-09-17 15:38:35 -0300

    Add files via upload

---
+++ Aulas e Desafios do Curso em Vídeo Gustavo Guanabára PYTHON/Desafio 046(Contagem regressiva).py
@@ -0,0 +1,8 @@
+import time
+
+print("\n\033[1;36m=== CONTAGEM REGRESSIVA PARA FOGOS DE ARTIFÍCIOS ===\033[m")
+
+for i in range(10, 0, -1):
+    print(i)
+    time.sleep(1)
+print("\033[1;35mOLHA OS FOGOS NO CÉU!!!!!!!!!\033[m")
---
+++ Aulas e Desafios do Curso em Vídeo Gustavo Guanabára PYTHON/Desafio 047(Numeros pares).py
@@ -0,0 +1,4 @@
+print('Número Pares')
+for i in range(0, 51, 2):
+    print(i)
+print("Fim")
